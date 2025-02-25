226711a8a7826937ae2116b371269b7c82ba594f
Xtremehead <Xtremehead@users.noreply.github.com>
2025-02-25 18:44:28 -0500

    Update input.js
    Signed-off-by: Xtremehead <Xtremehead@users.noreply.github.com>

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,3 +1,3 @@
-export default function input({ type = "text", className = "", ...props }) {
+export default function Input({ type = "text", className = "", ...props }) {
   return <input type={type} className={`border p-2 rounded ${className}`} {...props} />;
 }
